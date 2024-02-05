69a7a4c04189ea7822123a31e86cc9c9a4408a63
Ashish Sah <ashishsah1000@gmail.com>
2024-02-05 12:07:31 +0530

    create a basic frame

---
+++ BasicFrame.java
@@ -0,0 +1,14 @@
+import java.awt.Frame;
+
+public class BasicFrame {
+
+    public BasicFrame(String t) {
+        Frame fm = new Frame(t);
+        fm.setVisible(true);
+        fm.setSize(300, 500);
+    }
+
+    public static void main(String[] args) {
+        BasicFrame obj = new BasicFrame("Basic Frame");
+    }
+}
